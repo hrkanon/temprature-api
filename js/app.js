c07c73620e01c14ec2640620b787223387c7f1ae
hrkanon <habibur3753@gmail.com>
2021-09-03 16:34:35 +0600

    city temprature condition added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("abc");
+const apiKey = "8766f0a51c4a3e952a685fc490a3b76c";
+const searchTemperature = () => {
+  const city = document.getElementById("city-name").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTemperature(data));
+};
+
+const displayTemperature = (data) => {
+  console.log(data);
+  document.getElementById("city").innerText = `${data.name}`;
+  document.getElementById("temp").innerText = `${data.main.temp}`;
+  document.getElementById("condition").innerText = `${data.weather[0].main}`;
+};
